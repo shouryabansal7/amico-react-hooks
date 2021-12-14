99f5d64a6a7cbf23913863754e102f7d8830e9b9
Shourya Bansal <shouryab2001@gmail.com>
2021-12-14 14:05:27 +0530

    second commit : display posts and make api calls

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from '../utils';
+
+const customFetch = async (url, { body, ...customConfig }) => {
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+  const headers = {
+    'content-type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+  try {
+    const response = await fetch(url, config);
+    const data = await response.json();
+
+    if (data.success) {
+      return {
+        data: data.data,
+        success: true,
+      };
+    }
+
+    throw new Error(data.message);
+  } catch (err) {
+    console.error(err);
+    return {
+      message: err.message,
+      success: false,
+    };
+  }
+};
+
+export const getPosts = () => {
+  return customFetch(API_URLS.posts(), {
+    method: 'GET',
+  });
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Loader } from './';
+import { getPosts } from '../api';
+import { Home } from '../pages';
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Home posts={posts} />
     </div>
   );
 }
