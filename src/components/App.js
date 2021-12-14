76447163c3f8349a28d6ccd004d6d0d91e0e4a6e
Shourya Bansal <shouryab2001@gmail.com>
2021-12-14 15:35:53 +0530

    third commit : create navbar and add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Loader } from './';
+import React, { useEffect, useState } from 'react';
+import { Loader, Navbar } from './';
 import { getPosts } from '../api';
 import { Home } from '../pages';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -26,7 +31,21 @@ function App() {
 
   return (
     <div className="App">
-      <Home posts={posts} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <React.Fragment>
+                <Home posts={posts} />
+              </React.Fragment>
+            }
+          />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import styles from '../styles/navbar.module.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav}>
+      <div className={styles.leftDiv}>
+        <Link to="/">
+          <img
+            alt=""
+            src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+          />
+        </Link>
+      </div>
+
+      <div className={styles.rightNav}>
+        <div className={styles.user}>
+          <Link to="/">
+            <img
+              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+              alt=""
+              className={styles.userDp}
+            />
+          </Link>
+          <span>Aakash</span>
+        </div>
+
+        <div className={styles.navLinks}>
+          <ul>
+            <li>
+              <Link to="/">Log in</Link>
+            </li>
+            <li>
+              <Link to="/">Log out</Link>
+            </li>
+            <li>
+              <Link to="/">Register</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
