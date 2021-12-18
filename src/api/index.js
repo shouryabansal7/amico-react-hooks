bc47f55b096624f469ef7061a6b7e1d09eea0e37
Shourya Bansal <shouryab2001@gmail.com>
2021-12-18 13:40:35 +0530

    sixteenth commit : liking a post functionaity'

--- src/api/index.js
+++ src/api/index.js
@@ -113,3 +113,9 @@ export const createComment = async (content, postId) => {
     },
   });
 };
+
+export const toggleLike = (itemId, itemType) => {
+  return customFetch(API_URLS.toggleLike(itemId, itemType), {
+    method: 'POST',
+  });
+};
